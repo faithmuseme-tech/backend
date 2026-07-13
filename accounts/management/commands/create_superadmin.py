29883966cc5fe8cd0b10ce3bbfd694bf7c613022
Namatashabira <namatashabira@gmail.com>
2026-07-13 13:15:21 +0300

    chore: remove hardcoded credentials from code

--- accounts/management/commands/create_superadmin.py
+++ accounts/management/commands/create_superadmin.py
@@ -1,28 +1,33 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+from decouple import config
 
 User = get_user_model()
 
-ADMIN_EMAIL = 'primeaisle@admin.com'
-ADMIN_PASSWORD = 'primeaisle@admin2026'
-ADMIN_USERNAME = 'PrimeAisle'
-
 
 class Command(BaseCommand):
-    help = 'Create or update superadmin'
+    help = 'Create or update superadmin from environment variables'
 
     def handle(self, *args, **kwargs):
+        email = config('ADMIN_EMAIL', default='')
+        password = config('ADMIN_PASSWORD', default='')
+        username = config('ADMIN_USERNAME', default='admin')
+
+        if not email or not password:
+            self.stdout.write(self.style.WARNING('ADMIN_EMAIL or ADMIN_PASSWORD not set, skipping.'))
+            return
+
         user, created = User.objects.get_or_create(
-            email=ADMIN_EMAIL,
-            defaults={'username': ADMIN_USERNAME},
+            email=email,
+            defaults={'username': username},
         )
-        user.username = ADMIN_USERNAME
+        user.username = username
         user.is_staff = True
         user.is_superuser = True
         user.is_admin = True
         user.is_active = True
-        user.set_password(ADMIN_PASSWORD)
+        user.set_password(password)
         user.save()
 
         action = 'created' if created else 'updated'
-        self.stdout.write(self.style.SUCCESS(f'Superadmin {ADMIN_EMAIL} {action} successfully.'))
+        self.stdout.write(self.style.SUCCESS(f'Superadmin {email} {action} successfully.'))
