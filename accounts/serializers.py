14ce313aac385ecfe3897d7c5414464acb18fd69
Namatashabira <namatashabira@gmail.com>
2026-07-17 19:22:59 +0300

    Updated changes

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -12,7 +12,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'password2', 'first_name', 'last_name', 'phone')
+        fields = ('username', 'email', 'password', 'password2', 'first_name', 'last_name', 'phone', 'city', 'address')
 
     def validate(self, data):
         if data['password'] != data['password2']:
