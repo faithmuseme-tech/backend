88434012cd7ecdc0027174f319ad730323862389
Namatashabira <namatashabira@gmail.com>
2026-07-19 10:45:57 +0300

    Updated changes

---
+++ adminpanel/migrations/0001_site_settings.py
@@ -0,0 +1,19 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name='SiteSettings',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('seller_registration_open', models.BooleanField(default=True)),
+            ],
+            options={'verbose_name': 'Site Settings'},
+        ),
+    ]
--- adminpanel/models.py
+++ adminpanel/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class SiteSettings(models.Model):
+    seller_registration_open = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Site Settings"
+
+    @classmethod
+    def get(cls):
+        obj, _ = cls.objects.get_or_create(pk=1)
+        return obj
